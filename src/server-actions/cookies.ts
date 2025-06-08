60e0b7123199fbc6d6a35a811ba3fc5477b55ec1
akihito7 <akihitopro7@gmail.com>
2025-06-08 13:38:18 -0300

    feat : added response interceptor axios to when token is invalid redirect user to login

--- src/server-actions/cookies.ts
+++ src/server-actions/cookies.ts
@@ -16,3 +16,8 @@ export async function getCookie(key: string) {
   const cookieStore = await cookies();
   return cookieStore.get(key)?.value;
 }
+
+export async function deleteCookie(key: string) {
+  const cookieStore = await cookies();
+  cookieStore.delete(key)
+}
--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,5 +1,6 @@
-import { getCookie } from "@/server-actions/cookies";
+import { deleteCookie, getCookie } from "@/server-actions/cookies";
 import axios from "axios";
+import { toast } from "sonner";
 
 export const api = axios.create({
   baseURL: 'http://localhost:4010',
@@ -18,4 +19,27 @@ api.interceptors.request.use(
   },
   (error) => {
     return Promise.reject(error);
-  })
+  });
+
+
+api.interceptors.response.use(
+  async (config) => {
+    const token = await getCookie('@token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  async (error) => {
+
+    const statusCode = error.response.status;
+
+    if (statusCode === 401) {
+      await deleteCookie('@token');
+      window.location.href = '/login';
+    };
+
+    return Promise.reject(error);
+  });
+
+
